26ec1247e9abf50b26a3f30d0ffac23e7d778f9e
Goncalo <gcoroado@gmail.com>
2024-06-22 10:44:09 +0200

    Added StudentConfig Interface and 2 Students on the DB

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,10 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Constructor;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -10,16 +12,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Maria",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 
 }
